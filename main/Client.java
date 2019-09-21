443c7dd5b9e24c51b8916ca7e288cdf0f61a82b0
tnok_it_office_893 <48271049+massarfusion@users.noreply.github.com>
2019-09-21 21:32:40 +0800

    Add files via upload

---
+++ main/Client.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in =new Scanner (System.in);
+		int outSig=in.nextInt();
+		int max=in.nextInt();
+		CircleLink cl=new CircleLink();
+		for  (int i=1;i<=max;i++) {
+			
+			cl.add(i);
+			
+		}
+		cl.joseph(outSig);
+	}
+
+}
